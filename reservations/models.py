d953a640ef1294fc09475e17f6579970a6a836b6
chanhi <61691088+chanhi@users.noreply.github.com>
2022-08-01 20:02:44 +0900

    create other Apps

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Reservation(core_models.TimeStampedModel):
+
+    """Reservation Model Definition"""
+
+    STATUS_PENDING = "pending"
+    STATUS_CONFIRED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "Pending"),
+        (STATUS_CONFIRED, "Confirmed"),
+        (STATUS_CANCELED, "Canceled"),
+    )
+
+    Status = models.CharField(
+        max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    guest = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
